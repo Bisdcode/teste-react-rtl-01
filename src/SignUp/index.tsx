f5e86a73a1ce306042feddb7bb24fabeb0ef74e4
Bisdcode <bismarck.sdcode@gmail.com>
2025-10-08 15:45:21 -0300

    apply tdd tests in SignUp component

---
+++ src/SignUp/index.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom"
+import styles from "./styles.module.scss"
+import type { FormEvent } from "react";
+
+export default function SignUp() {
+    const navigate = useNavigate();
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        navigate("/dashboard");
+    }
+
+    return (
+        <div className={styles.container}>
+            <h2>Cadastre-se</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Insira seu nome"/>
+                <input type="text" placeholder="Insira seu e-mail"/>
+                <input type="text" placeholder="Insira sua senha"/>
+            <button>Sign Up</button>
+            </form>
+        </div>
+    )
+}
