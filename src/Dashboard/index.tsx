f71a043afbcd0735f908d8fb5bfe3272e27e1274
Bisdcode <bismarck.sdcode@gmail.com>
2025-10-10 21:01:17 -0300

    test api pokemons

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react"
 import styles from "./styles.module.css"
+import type { PokemonType } from "../types/PokemonType"
 
-export default function Dashboard() {
+interface IProps {
+    fetchPokemonList: () => Promise<PokemonType[]>;
+}
+
+export default function Dashboard({fetchPokemonList}: IProps) {
+    const [pokemons, setPokemons] = useState<PokemonType[]>([]);
+    
+    useEffect(() => {
+        (async () => {
+            const data = await fetchPokemonList();
+            setPokemons(data);
+        })()
+    }, []);
+
     return (
         <div className={styles.container}>
             <h1>Dashboard</h1>
+
+            <ul className={styles["container-pokemons"]}>
+                {pokemons.map((pokemons) => (
+                    <li key={pokemons.id}>
+                        <h1>{pokemons.name}</h1>
+                        <img src={pokemons.image} alt={pokemons.name} />
+                        <strong>{pokemons.type}</strong>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import SignUp from "./SignUp";
+import { fetchPokemonList } from "./services/PokemonServices";
 
 export default function MainRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Login/>}/>
-            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/dashboard" element={<Dashboard fetchPokemonList={fetchPokemonList}/>}/>
             <Route path="/sign-up" element={<SignUp/>}/>
 
             <Route path="*" element={<h1>404 Page Not Found</h1>}/>
